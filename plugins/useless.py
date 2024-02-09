8f7efe64a9b19cd1f0a864590daf15ffd5b4b4e6
yakho1432 <153206694+yakho1432@users.noreply.github.com>
2024-02-09 11:08:08 +0530

    Update useless.py

--- plugins/useless.py
+++ plugins/useless.py
@@ -4,6 +4,23 @@ from pyrogram import filters
 from config import ADMINS, BOT_STATS_TEXT, USER_REPLY_TEXT
 from datetime import datetime
 from helper_func import get_readable_time
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+
+@Bot.on_message(filters.command('join') & filters.private)
+async def followus(bot: Bot, message: Message):
+    reply_markup=InlineKeyboardMarkup(
+                        [
+                         [
+                          InlineKeyboardButton('🎬 𝑴𝒐𝒗𝒊𝒆𝒔 𝒈𝒓𝒐𝒖𝒑', url="t.me/+LwC2E43TvHljZWM1"),
+                          InlineKeyboardButton('🥹 𝑼𝒑𝒅𝒂𝒕𝒆𝒔 𝑪𝒉𝒂𝒏𝒏𝒆𝒍', url="t.me/MoviezAddaKA")
+                       ],[
+                          InlineKeyboardButton("🧑‍💻 𝑩𝒐𝒕 𝑪𝒓𝒆𝒂𝒕𝒆𝒓", url="t.me/captblacknight")
+                         ]
+                        ]
+                    )
+    await message.reply(f"<b> ⭐ ᴄʟɪᴄᴋ ʙᴇʟᴏᴡ ʙᴜᴛᴛᴏɴs ᴛᴏ ᴊᴏɪɴ ᴜꜱ ⭐</b>\n\n", reply_markup=reply_markup, disable_web_page_preview = True)
+
+
 
 @Bot.on_message(filters.command('stats') & filters.user(ADMINS))
 async def stats(bot: Bot, message: Message):
